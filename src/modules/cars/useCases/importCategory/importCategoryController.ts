fcfb9fc0b003447ca41ec352d36df8fbf4f9fafb
yan-levy <yanbatistacel@gmail.com>
2022-08-04 01:32:45 -0400

    Working with Upload - finished

---
+++ src/modules/cars/useCases/importCategory/importCategoryController.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from "express";
+
+import { ImportCategoryUseCase } from "./importCategoryUseCase";
+
+class ImportCategoryController {
+    constructor(private importCategoryUsecase: ImportCategoryUseCase) {}
+    handle(request: Request, response: Response): Response {
+        const { file } = request;
+
+        this.importCategoryUsecase.execute(file);
+
+        return response.send();
+    }
+}
+
+export { ImportCategoryController };
